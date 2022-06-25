3550739f14613b141d59a51b83428e7bb9c3ad45
Dima <Dimaboyko2017@mail.ru>
2022-06-25 23:43:42 +0300

    Добавлена структура графа в виде матрицы инцидентности

--- main.c
+++ main.c
@@ -192,3 +192,60 @@ void print_graph(graph *g){
     }
 
 }
+
+typedef struct mgraph{
+    int n;
+    int *address;
+}mgraph;
+
+mgraph *minit(int n){
+    mgraph *new_graph= malloc(sizeof(mgraph));
+    new_graph->n=n;
+    int *address = malloc(n*n*sizeof(int));
+    int i;
+    for(i=0;i<n*n;i++) address[i]=0;
+    new_graph->address=address;
+    return new_graph;
+}
+
+void madd_edge(mgraph *g, int from, int to){
+    if (from>=g->n||to>=g->n) return;
+    int n1=g->n*from+to;
+    int n2=g->n*to+from;
+    int *add=g->address;
+    add[n1]=1;
+    add[n2]=1;
+}
+
+void mdel_edge(mgraph *g, int from, int to){
+    if (from>=g->n||to>=g->n) return;
+    int n1=g->n*from+to;
+    int n2=g->n*to+from;
+    int *add =g->address;
+    add[n1]=0;
+    add[n2]=0;
+}
+
+int power(mgraph *g, int v){
+    int n=g->n;
+    int *add=g->address;
+    int i;
+    int p=0;
+    for(i=0;i<n;i++) p=p+add[v*n+i];
+    return p;
+}
+
+void print_gr(mgraph *g){
+    unsigned int n = g->n;
+    int i;
+    printf("  ");
+    for (i=0; i<n; i++) printf("%d ",i);
+    printf("\n0 ");
+    for(i=0; i<n*n; i++){
+        printf("%d ", g->address[i]);
+        if ((i+1)%n==0) {
+            printf("\n");
+            if (i != n * n - 1) printf("%d ", (i + 1) / n);
+        }
+    }
+}
