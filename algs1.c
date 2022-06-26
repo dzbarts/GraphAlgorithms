19860e79efe0f87a4c1b1d62f9178cd3b37940af
Dima <Dimaboyko2017@mail.ru>
2022-06-26 21:32:00 +0300

    Реализован алгоритм Беллмана-Форда

--- algs1.c
+++ algs1.c
@@ -45,3 +45,52 @@ void BFS(graph *g, int number){
 
 }
 
+//алгоритм Белмана-Форда
+double *Belman_Ford(graph *g, int vertex){
+    adj_list *current;
+    pair p;
+    int n=0;
+    int number;
+    int curr;
+    int dist;
+    double distance;
+    graph *gr=g;
+    while(gr){
+        n++;
+        gr=gr->next;
+    }
+    double *res= malloc(n * sizeof(double));
+    int i;
+    for(i=0;i<n;i++){
+        if(i==vertex) res[i]=0;
+        else res[i]=10000;
+    }
+    adj_list *list2=NULL;
+    p.number=vertex;
+    p.distance=0;
+    list2=add(list2,p);
+    adj_list *list=list2;
+    for(i=0;i<n-1;i++){
+        list2=NULL;
+        while(list){
+            curr=list->vertex.number;
+            printf("%d ",curr);
+            current = get_vertex(g, curr)->vertexes;
+            while(current){
+                number=current->vertex.number;
+                distance=current->vertex.distance;
+                dist=res[curr]+distance;
+                if(dist<res[number]) res[number]=dist;
+                current=current->next;
+                p.number=number;
+                p.distance=0;
+                list2=add(list2,p);
+            }
+            list=list->next;
+        }
+        list=list2;
+        printf("\n");
+    }
+    return res;
+}
+
