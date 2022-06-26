c88cb5bafd0e561900231a823aea1b439c921578
Dima <Dimaboyko2017@mail.ru>
2022-06-26 15:54:41 +0300

    Добавлена функция поиска эйлерова пути в графе и вспомагательные функции для нее: проверка на эйлероваость и поиск эйлерова пути из заданой вершины. Также добавлены некоторые комментарии

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//структура пара, в которой будет храниться номер сежной вершины и расстояние до нее
 typedef struct pair{
     unsigned int number;
     double distance;
 }pair;
 
+//структура список смежных вершин
 typedef struct adj_list{
     pair vertex;
     struct adj_list *next;
 }adj_list;
 
+//функция добавления элемента в список смежных вершин
 adj_list *add(adj_list *list, pair new_pair){
     if (!list){
         adj_list *new_list= (malloc(sizeof(adj_list)));
@@ -31,6 +34,7 @@ adj_list *add(adj_list *list, pair new_pair){
     return list;
 }
 
+//функция удаления элемента из списка смежных вершин
 adj_list *del(adj_list *list, unsigned int number){
     if (!list) return list;
     adj_list *current=list;
@@ -49,6 +53,7 @@ adj_list *del(adj_list *list, unsigned int number){
     return list;
 }
 
+//функция полного удаления списка смежных вершин
 adj_list *del_list(adj_list *list){
     if (!list) return list;
     adj_list *current=list;
@@ -61,14 +66,16 @@ adj_list *del_list(adj_list *list){
     return NULL;
 }
 
+//структура граф в виде списков смежности
 typedef struct graph{
-    unsigned char is_empty;
-    unsigned int number;
-    unsigned char status;
-    adj_list *vertexes;
-    struct graph *next;
+    unsigned char is_empty;//поле, позволяющее определить пустой ли граф
+    unsigned int number;//номер вершины
+    unsigned char status;//ствтус вершины
+    adj_list *vertexes;//список смежных вершин для данной вершины
+    struct graph *next;//ссылка на следующую вершину графа
 }graph;
 
+//функция инициализации графа
 graph *init(){
     graph *new_graph= (malloc(sizeof(graph)));
     new_graph->is_empty=1;
@@ -77,6 +84,7 @@ graph *init(){
     return new_graph;
 }
 
+//функция добавления вершины в граф
 void add_vertex(graph *g, unsigned int number, unsigned char status){
     if (g->is_empty){
         g->is_empty=0;
@@ -96,6 +104,7 @@ void add_vertex(graph *g, unsigned int number, unsigned char status){
     current->next=new_g;
 }
 
+//функция добавления ребра в граф
 void add_edge(graph *g, unsigned int from, unsigned int to, double distance){
     graph *current = g;
     pair pair0;
@@ -126,6 +135,7 @@ void add_edge(graph *g, unsigned int from, unsigned int to, double distance){
     }
 }
 
+//функция удаления ребра из графа
 void del_edge(graph *g, unsigned int from, unsigned int to){
     graph *current=g;
     while(current->next&&current->number!=from) current=current->next;
@@ -133,6 +143,7 @@ void del_edge(graph *g, unsigned int from, unsigned int to){
     current->vertexes=del(current->vertexes, to);
 }
 
+//функция удаления вершины из графа
 void del_vertex(graph *g, unsigned int number){
     graph *current=g;
     graph *prev=g;
@@ -173,6 +184,7 @@ void del_vertex(graph *g, unsigned int number){
     }
 }
 
+//функция печати графа на экран
 void print_graph(graph *g){
     graph *current=g;
     if(current->is_empty){
@@ -193,6 +205,7 @@ void print_graph(graph *g){
 
 }
 
+//функция обнуления статуса всех вершин графа
 void zero_status(graph *g){
     graph *current=g;
     while (current){
@@ -201,12 +214,14 @@ void zero_status(graph *g){
     }
 }
 
+//функция получения адреса вершины в шрафе
 graph *get_vertex(graph *g, unsigned int number){
     graph *current=g;
     while(current&&current->number!=number) current=current->next;
     return current;
 }
 
+//алгоритм поиска в глубину в графе
 void DFS(graph *g, int number){
     graph *start=get_vertex(g,number);
     adj_list *current;
@@ -222,6 +237,7 @@ void DFS(graph *g, int number){
     else return;
 }
 
+//алгоритм поиска в ширину в графе
 void BFS(graph *g, int number){
     graph *start=get_vertex(g,number);
     adj_list *current;
@@ -248,11 +264,13 @@ void BFS(graph *g, int number){
 
 }
 
+//структура графа в виде матрицы инцидентности
 typedef struct mgraph{
     int n;
     int *address;
 }mgraph;
 
+//функция инициализации графа
 mgraph *minit(int n){
     mgraph *new_graph= malloc(sizeof(mgraph));
     new_graph->n=n;
@@ -263,6 +281,7 @@ mgraph *minit(int n){
     return new_graph;
 }
 
+//функция добавления ребра в графе
 void madd_edge(mgraph *g, int from, int to){
     if (from>=g->n||to>=g->n) return;
     int n1=g->n*from+to;
@@ -272,6 +291,7 @@ void madd_edge(mgraph *g, int from, int to){
     add[n2]=1;
 }
 
+//функция удаления ребра в графе
 void mdel_edge(mgraph *g, int from, int to){
     if (from>=g->n||to>=g->n) return;
     int n1=g->n*from+to;
@@ -281,6 +301,7 @@ void mdel_edge(mgraph *g, int from, int to){
     add[n2]=0;
 }
 
+//функция получения степени вершины графа
 int power(mgraph *g, int v){
     int n=g->n;
     int *add=g->address;
@@ -290,6 +311,7 @@ int power(mgraph *g, int v){
     return p;
 }
 
+//функция печати графа на экран
 void print_gr(mgraph *g){
     unsigned int n = g->n;
     int i;
@@ -305,16 +327,19 @@ void print_gr(mgraph *g){
     }
 }
 
+//структура стека
 typedef struct stack{
     int n;
     int address[100];
 }stack;
 
+//функция инициализации стека
 stack *create(){
     stack *s= malloc(sizeof(stack));
     s->n=0;
 }
 
+//функция push
 int push(stack *s, int value){
     int n=s->n;
     *(s->address+n)=value;
@@ -322,6 +347,7 @@ int push(stack *s, int value){
     return value;
 }
 
+//функция pop
 int pop(stack *s){
     int n=s->n;
     if(!n) return -1;
@@ -330,6 +356,7 @@ int pop(stack *s){
     return value;
 }
 
+//функция выводв стека на экран
 void print_stack(stack *s){
     int i;
     int n=s->n;
@@ -339,4 +366,63 @@ void print_stack(stack *s){
     }
     int *add=s->address;
     for(i=0;i<n;i++) printf("%d ",add[i]);
+}
+
+//проверка графа на эйлеровость
+int check_euler(mgraph *g){
+    int count=0;
+    int n=g->n;
+    int i;
+    for(i=0;i<n;i++){
+        if(power(g,i)%2) count++;
+        if(count>2) return 0;
+    }
+    if(count==1) return 0;
+    if(count==2) return 2;
+    return 1;
+
+}
+
+//поиск эйлерова пути из заданой вершины(вспомогательная функция для поиска эйлерова пути в графе)
+void euler_path0(mgraph *g, int start, stack *l, stack *c){
+    int n=g->n;
+    int i=0;
+    int t=1;
+    int *add=g->address;
+    push(l,start);
+    while(t&&i<n) {
+        if (add[n*start + i]) {
+            t=0;
+            add[start*n+i]=0;
+            add[i*n+start]=0;
+            euler_path0(g,i,l,c);
+        }
+        i++;
+    }
+    if(t){
+        push(c, pop(l));
+        if(l->n) euler_path0(g,pop(l),l,c);
+    }
+}
+
+//функция поиска эйлерова пути в графе
+stack *euler_path(mgraph *g){
+    int t= check_euler(g);
+    int start;
+    stack *c=create();
+    if(!t) return c;
+    stack *l=create();
+    if(t==1) start=0;
+    else{
+        mgraph *gr=g;
+        int i=0;
+        int f=1;
+        while(power(g,f)%2==0){
+            i++;
+        }
+        start=i;
+    }
+    euler_path0(g,start,l,c);
+    free(l);
+    return c;
 }
